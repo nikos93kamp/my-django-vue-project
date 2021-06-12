f91157263175299bbd6ecbb67959ba0589790dd3
Nikos Kampouridis <nkampouridis9@gmail.com>
2021-06-12 15:12:45 +0300

    Initial commit

---
+++ client/client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+
+    <Container />
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ djvueproject/restapi/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Blogger(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=500)
